d279a0f0bc3dc5e6a9938ee4a038bdb1a9ab2a62
이원섭wonsup Lee <onesup.lee@gmail.com>
2020-01-28 04:57:30 +0900

    겹치는 방 id 는 갱신
    list =
    %w(174671016
    243641381
    228400518
    209586338)
    list.each{|x| RecordedRoomsService.new(x).record }

--- app/services/recorded_rooms_service.rb
+++ app/services/recorded_rooms_service.rb
@@ -8,18 +8,26 @@ class RecordedRoomsService
 
   def record
     service = PostedRoomsService.new(abnb_user_id)
-    service.listing.each do |listed_room|
+    listing = service.listing
+    listing.each do |listed_room|
       create_snapshoted_room(listed_room)
     end
+    listing.map{ |x| x["id"] }
   end
 
-  def create_snapshoted_room(listed_room)
+  def create_snapshoted_room(listed_room)    
     room = SnapshotedRoom.new
+    if SnapshotedRoom.where(ab_room_id: listed_room["id"]).exists?
+      room = SnapshotedRoom.where(ab_room_id: listed_room["id"]).first
+    end
     room.name = listed_room["name"]
-    room.ab_room_id = listed_room["room_id"]
-    room.ab_user_id = listed_room["user_id"]
+    room.ab_room_id = listed_room["id"]
+    user_id = listed_room["user_id"]
+    user_id = abnb_user_id unless user_id.present?
+    room.ab_user_id = user_id
     room.snapshot_data = listed_room
+    room.bedrooms = listed_room["bedrooms"]
     room.localized_city = listed_room["localized_city"]
-    room.save
+    room.save!
   end
 end
--- test/services/recorded_rooms_service_test.rb
+++ test/services/recorded_rooms_service_test.rb
@@ -8,6 +8,6 @@ class RecordedRoomsServiceTest < ActiveSupport::TestCase
   test "특정 호스트의 전체 방 정보 저장하기" do  
     abnb_user_id = 121759778
     service = RecordedRoomsService.new(abnb_user_id)
-    service.record    
+    service.record
   end
 end
